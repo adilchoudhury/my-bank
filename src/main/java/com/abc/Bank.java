49f1f14601b999d4ee038ddee6e8fc258eb9b0f3
adilchoudhury <adil.choudhury@accenture.com>
2017-03-14 10:13:53 +0000

    Refactored + 2 Additional Features
    The 2 Features are Maxi Savings Interest Rate change dependant on withdrawal and transferring between accounts

--- src/main/java/com/abc/Bank.java
+++ src/main/java/com/abc/Bank.java
@@ -16,8 +16,9 @@ public class Bank {
 
     public String customerSummary() {
         String summary = "Customer Summary";
-        for (Customer c : customers)
+        for (Customer c : customers) {
             summary += "\n - " + c.getName() + " (" + format(c.getNumberOfAccounts(), "account") + ")";
+        }
         return summary;
     }
 
@@ -29,18 +30,29 @@ public class Bank {
 
     public double totalInterestPaid() {
         double total = 0;
-        for(Customer c: customers)
+        for(Customer c: customers) {
             total += c.totalInterestEarned();
+        }
         return total;
     }
 
     public String getFirstCustomer() {
         try {
-            customers = null;
             return customers.get(0).getName();
         } catch (Exception e){
             e.printStackTrace();
             return "Error";
+        }
+    }
+    //Added Feature Change Maxi-Savings accounts 
+    public void endOfDay(){
+        try {
+            for (Customer c : customers){
+                c.endOfDay();
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+            System.out.println("No Customers in Bank");
         }
     }
 }
